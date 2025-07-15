0af1804d87894c73ccdf286871df17b548655847
ADITYA_0093 <aditya.goyalsmart@gmail.com>
2025-07-15 09:35:18 +0530

    updated user login schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,9 @@
-import mongoose from 'mongoose';
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true},
-    role: {type: String, enum: ['admin', 'user'], default: 'user'},
-    assignedQuizzes: [{type: mongoose.Schema.Types.ObjectId, ref: 'Quiz' }]
-})
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
